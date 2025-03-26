4bcfee22a69de118b4bc766b1f441fe4c6de9301
HamzaAhmad6292 <hamzaahmad6292@gmail.com>
2025-03-26 21:33:14 +0500

    resolving commit history

--- lib/apis/reportingApis.ts
+++ lib/apis/reportingApis.ts
@@ -1,7 +1,7 @@
 // reportingService.ts
 import axios, { type AxiosError, type AxiosResponse } from "axios"
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api/v1"
+const API_BASE_URL = "http://127.0.0.1:3001/api/v1"
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
